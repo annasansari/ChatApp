6822f297a479d92d1519f14d362ebebcc41c5f4f
Anas Ansari <121580678+annasansari@users.noreply.github.com>
2023-11-25 18:55:12 +0500

    Make a global variable using Context Api

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,13 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import SignUp from './pages/SignUp'
 import SignIn from './pages/SignIn'
 import Home from './pages/Home'
+import { useContext } from 'react'
+import { AuthContext } from './context/AuthContext'
 
 function App() {
+
+  // const { currentUser } = useContext(AuthContext)
+  // console.log(currentUser);
 
   return (
     <>
